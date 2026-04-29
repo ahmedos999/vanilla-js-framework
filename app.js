d6a4296111a212705e613d4ddcf6214bccc3693e
Ahmed Sheikh <ahmedos099912@gmail.com>
2026-04-29 19:43:41 +0400

    Adding globale state

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+import { loadData } from "./services/Menu.js"
 import { Router } from "./services/router.js"
+import store from "./services/store.js"
+
+window.app = {}
+app.router = Router
+app.store = store
 
 
 document.addEventListener("DOMContentLoaded",()=>{
-    Router.init()
+    app.router.init()
+
+    loadData();
 })
